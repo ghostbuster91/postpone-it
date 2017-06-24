610564aa8f47f7b9ff03fc6839c197da3d909f0a
ghostbuster <kghost0@gmail.com>
2017-06-24 14:54:00 +0200

    Add time&date pickers

--- app/src/main/java/io/github/ghostbuster91/postponeit/CreateDelayedActivity.kt
+++ app/src/main/java/io/github/ghostbuster91/postponeit/CreateDelayedActivity.kt
@@ -9,7 +9,10 @@ import com.jakewharton.rxbinding2.view.clicks
 import com.tbruyelle.rxpermissions2.RxPermissions
 import com.trello.rxlifecycle2.components.RxActivity
 import com.trello.rxlifecycle2.kotlin.bindToLifecycle
+import com.wdullaer.materialdatetimepicker.date.DatePickerDialog
+import com.wdullaer.materialdatetimepicker.time.TimePickerDialog
 import kotlinx.android.synthetic.main.create_delayed_layout.*
+import java.util.*
 
 class CreateDelayedActivity : RxActivity() {
 
@@ -19,7 +22,7 @@ class CreateDelayedActivity : RxActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.create_delayed_layout)
         val requestSmsPermission = RxPermissions(this).request(Manifest.permission.SEND_SMS)
-        sendSmsButton.clicks()
+        scheduleButton.clicks()
                 .bindToLifecycle(this)
                 .flatMap { requestSmsPermission }
                 .subscribe { granted ->
@@ -29,6 +32,36 @@ class CreateDelayedActivity : RxActivity() {
                         Toast.makeText(this@CreateDelayedActivity, "not granted", Toast.LENGTH_LONG).show()
                     }
                 }
+        timeInput.setOnClickListener {
+            showTimePicker()
+        }
+        dateInput.setOnClickListener {
+            showDatePicker()
+        }
+    }
+
+    private fun showDatePicker() {
+        val now = Calendar.getInstance()
+        val dpd = DatePickerDialog.newInstance(
+                { v, y, m, d ->
+                    dateInput.setText("$d/$m/$y")
+                },
+                now.get(Calendar.YEAR),
+                now.get(Calendar.MONTH),
+                now.get(Calendar.DAY_OF_MONTH))
+        dpd.show(fragmentManager, "Datepickerdialog")
+    }
+
+    private fun showTimePicker() {
+        val now = Calendar.getInstance()
+        val dpd = TimePickerDialog.newInstance(
+                { v, h, m, s ->
+                    timeInput.setText("$h:$m")
+                },
+                now.get(Calendar.HOUR),
+                now.get(Calendar.MINUTE),
+                true)
+        dpd.show(fragmentManager, "Datepickerdialog")
     }
 
     private fun onSmsClick() {
