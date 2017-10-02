2715377309fae3e95ac59f405b094df3cbb14420
ghostbuster91 <kghost0@gmail.com>
2017-10-02 22:02:36 +0200

    #8 Display notification about executed jobs

--- app/src/main/java/io/github/ghostbuster91/postponeit/job/execute/SendSmsJobExecutor.kt
+++ app/src/main/java/io/github/ghostbuster91/postponeit/job/execute/SendSmsJobExecutor.kt
@@ -1,13 +1,18 @@
 package io.github.ghostbuster91.postponeit.job.execute
 
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
+import android.support.v7.app.NotificationCompat
 import android.telephony.SmsManager
 import android.util.Log
+import io.github.ghostbuster91.postponeit.R
 import io.github.ghostbuster91.postponeit.job.DelayedJobStatus
 import io.github.ghostbuster91.postponeit.job.jobServiceProvider
+import io.github.ghostbuster91.postponeit.job.list.JobListActivity
 
 class SendSmsJobExecutor : BroadcastReceiver() {
 
@@ -20,10 +25,23 @@ class SendSmsJobExecutor : BroadcastReceiver() {
         smsManager.sendTextMessage(delayedJob.number, null, delayedJob.text, null, null)
         jobService.updateJob(delayedJob.copy(status = DelayedJobStatus.EXECUTED))
         Log.d(SendSmsJobExecutor::class.java.name, "$delayedJob exeuted")
+        val notificationClickIntent = PendingIntent.getActivity(context, 0, Intent(context, JobListActivity::class.java), 0)
+        val notification = createNotification(context, notificationClickIntent, "Sms sent to ${delayedJob.number}", delayedJob.text)
+        val mNotificationManager = context!!.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        mNotificationManager.notify(0, notification)
     }
 
+    private fun createNotification(context: Context?, notificationClickIntent: PendingIntent?, title: String, text: String) =
+            NotificationCompat.Builder(context)
+                    .setSmallIcon(R.drawable.hourglass_icon)
+                    .setContentTitle(title)
+                    .setContentText(text)
+                    .setAutoCancel(true)
+                    .setContentIntent(notificationClickIntent)
+                    .build()
+
     companion object {
-        const private val KEY = "SMS_DATA_KEY"
+        private const val KEY = "SMS_DATA_KEY"
         fun intent(context: Context, delayedJobId: Int) =
                 Intent(context, SendSmsJobExecutor::class.java)
                         .setData(Uri.parse(delayedJobId.toString()))
