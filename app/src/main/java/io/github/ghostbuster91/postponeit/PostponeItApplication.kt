e2c5b394bf45c4378a8fbeb9e95e9c7d50c6072d
ghostbuster91 <kghost0@gmail.com>
2018-01-28 12:39:20 +0100

    Setup crashlytics

--- app/src/main/java/io/github/ghostbuster91/postponeit/PostponeItApplication.kt
+++ app/src/main/java/io/github/ghostbuster91/postponeit/PostponeItApplication.kt
@@ -3,9 +3,12 @@ package io.github.ghostbuster91.postponeit
 import android.app.Application
 import android.content.Context
 import android.content.SharedPreferences
+import com.crashlytics.android.Crashlytics
+import com.crashlytics.android.core.CrashlyticsCore
 import com.github.salomonbrys.kodein.*
 import com.github.salomonbrys.kodein.android.androidModule
 import com.google.gson.Gson
+import io.fabric.sdk.android.Fabric
 import io.github.ghostbuster91.postponeit.job.*
 import io.github.ghostbuster91.postponeit.job.execute.NotificationService
 import io.github.ghostbuster91.postponeit.job.execute.NotificationServiceImpl
@@ -24,5 +27,13 @@ class PostponeItApplication : Application(), KodeinAware {
         bind<JobService>() with singleton {
             JobServiceImpl(alarmManagerService = instance(), jobRepository = instance())
         }
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        val crashlyticsCore = CrashlyticsCore.Builder()
+                .disabled(BuildConfig.DEBUG)
+                .build()
+        Fabric.with(this, Crashlytics.Builder().core(crashlyticsCore).build())
     }
 }
